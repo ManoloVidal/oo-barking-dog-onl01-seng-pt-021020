f5b369e4983eaafd3e20d129c4a103f42865ab6b
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-24 16:25:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
+class Dog 
+  def name=(dog_name)
+    @this_dogs_name = dog_name 
+  end 
+  
+  def name
+    @this_dogs_name
+    
+  def bark
+    puts "woof!"
+  end 
+end 
 
+lassie = Dog.new 
+lassie.name = "Lassie"
+puts lassie.name 
