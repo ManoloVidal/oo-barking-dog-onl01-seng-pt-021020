edf6c2e2486f14599ebd14a4d4aba70d84ef899a
Juan Vidal-Ramos <vidalramosjm@gmail.com>
2020-02-24 16:29:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,14 @@
 class Dog 
-  def.name=(dogs_name)
-  @This_dogs_name = dogs_name 
+  def name=(dogs_name)
+  @this_dogs_name = dogs_name 
 end
-  def
+  def name
+    @this_dogs_name
+  end
+def bark
+  puts "woof!"
+end
+end
+lassie = Dog.new 
+lassie.name
+lassie.bark 
